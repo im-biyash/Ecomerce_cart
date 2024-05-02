546513a93e03ddce3608b12020b1b0be755d699f
biyash <bsstha21@gmail.com>
2024-05-02 21:58:33 +0545

    cahnge

--- src/Componenets/Products.jsx
+++ src/Componenets/Products.jsx
@@ -31,7 +31,7 @@ function Products() {
   
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="grid grid-cols-3 md:grid-cols-3 gap-4">
       {products.map((product) => (
         <div
           key={product.id}
